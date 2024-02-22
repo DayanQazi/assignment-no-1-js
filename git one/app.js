de9873a01eb3e62b930acd2b490a8aca6198aaa7
Dayan Qazi <dayanqazi622@gamil.com>
2024-02-23 04:12:43 +0500

    work done

---
+++ git one/app.js
@@ -0,0 +1,42 @@
+var num1 = +prompt("Enter your 1st value")
+var num2 = +prompt("Enter your 2nd value")
+
+
+var add = num1 + num2;
+var minus = num1 - num2;
+var sub = num1 * num2;
+var divi = num1 / num2;
+
+
+
+
+document.write(
+    "<table border='1'>"+
+    "<tr>"+
+    "<th>"+"ADDITION"+"</th>"
+    +"<th>"+"MINUS"+"</th>"
+    +"<th>"+"SUB"+"</th>"
+    +"<th>"+"DIVI"+"</th>"
+   
+   
+    +"</tr>"
+
+    +"<tr>"+
+    "<td>"+add+"</td>"
+    +"<td>"+minus+"</td>"
+    +"<td>"+sub+"</td>"
+    +"<td>"+divi+"</td>"
+    
+    
+
+   
+   
+    +"</tr>"
+    
+    
+    
+    
+    +"</table>"
+
+
+)
